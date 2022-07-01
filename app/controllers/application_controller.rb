b82f49ae38a5177707c7fc9cf9f7998613c433c3
Eli Craig <elidcraig@gmail.com>
2022-07-01 10:34:17 -0700

    error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
 
+  def handle_invalid e
+    render json: { errors: ["validation errors"]}, status: 422
+  end
+
+  def handle_not_found e
+    render json: { error: "#{e.model} not found" }, status: 404
+  end
 end
