ae234d4a3e638c6df2e0a273ec7c9da54b9267e0
Eli Craig <elidcraig@gmail.com>
2022-07-01 10:16:06 -0700

    camper validations

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,4 +1,7 @@
 class Camper < ApplicationRecord
+  validates :name, :age, presence: true
+  validates :age, inclusion: { in: 8..18 }
+
   has_many :signups
   has_many :activities, through: :signups
 end
