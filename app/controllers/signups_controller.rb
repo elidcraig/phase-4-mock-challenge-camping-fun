39bd4aee9932524fc4fee3aeb83cb22abef7e9e5
Eli Craig <elidcraig@gmail.com>
2022-07-01 10:39:55 -0700

    signups controller actions

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,12 @@
 class SignupsController < ApplicationController
+  def create
+    new_signup = Signup.create!(signup_params)
+    render json: new_signup, status: :created
+  end
+
+  private
+
+  def signup_params
+    params.permit(:time, :camper_id, :activity_id)
+  end
 end
